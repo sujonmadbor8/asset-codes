d98028f68a2332995d7aa09c312a87e4578b8e63
Sujon <sujonmadbor8@gmail.com>
2025-09-02 20:45:45 +0600

    added

--- JavaScript/DOM procoder/js/script.js
+++
@@ -1,8 +0,0 @@
-let hero = document.querySelector(".hero");
-let readBtn = document.createElement("button");
-
-readBtn.innerText = "Read More";
-readBtn.classList.add("readbtn");
-readBtn.style.backgroundColor = "#000";
-
-hero.append(readBtn);
